ae7d970eb6b2be317d40dbb9abc3000b833a15af
tstjean <taylorst@uw.edu>
2025-08-26 14:55:09 -0400

    correction: bump version to 0.8.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 
 project = 'IceGraph'
 author = 'Taylor St Jean'
-release = '0.8.2'
+release = '0.8.3'
 
 html_theme = "sphinx_rtd_theme"
 master_doc = 'index'
